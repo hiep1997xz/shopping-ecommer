bfa238fe14927f52e3f43553fdcdb88478fd3a0a
hiep1997xz <hiep1997xz@gmail.com>
2022-05-31 13:55:15 +0700

    top-categories

--- src/pages/Pages.jsx
+++ src/pages/Pages.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Home from '../components/MainPage/Home'
 import FlashDeals from "../components/flashDeals/FlashDeals"
+import TopCate from '../components/TopCategories/TopCate'
 
 
 const Pages = ({productItems, addToCart, shopItems, CartItem}) => {
@@ -8,6 +9,7 @@ const Pages = ({productItems, addToCart, shopItems, CartItem}) => {
     <>
       <Home CartItem={CartItem}/>
       <FlashDeals productItems={productItems} addToCart={addToCart} />
+      <TopCate />
     </>
   )
 }
